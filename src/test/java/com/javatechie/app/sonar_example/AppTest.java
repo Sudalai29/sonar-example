3fd707182404f7a7b53dfb44e3ecb02557d318f0
Sudalai Madan <62635527+Sudalai29@users.noreply.github.com>
2024-11-20 12:55:30 +0530

    Update AppTest.java

--- src/test/java/com/javatechie/app/sonar_example/AppTest.java
+++ src/test/java/com/javatechie/app/sonar_example/AppTest.java
@@ -1,38 +1,21 @@
 package com.javatechie.app.sonar_example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
-	/**
-	 * Create the test case
-	 *
-	 * @param testName
-	 *            name of the test case
-	 */
-	public AppTest(String testName) {
-		super(testName);
-	}
+public class AppTest {
 
-	/**
-	 * @return the suite of tests being tested
-	 */
-	public static Test suite() {
-		return new TestSuite(AppTest.class);
-	}
-
-	/**
-	 * Rigourous Test :-)
-	 */
-	public void testApp() {
-
-		App a = new App();
-		a.m1();
-		a.getData();
-		assertTrue(true);
-	}
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testApp() {
+        App a = new App();
+        a.m1();
+        a.getData();
+        assertTrue(true);
+    }
 }
